56fe136d0abd03eed5806c8f67d0f5ddbec3cd2b
Ysabel Dela Cruz <delacruz.ysa@gmail.com>
2023-01-27 12:31:50 +0800

    fixed DB

--- db/conn.js
+++ db/conn.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const db = process.env.MONGO_URI;
 
 mongoose.connect(db, {
-  useUnifiedTopology:true,
-  useNewUrlParser:true
-  }).then(() => console.log("Connected to MongoDB"))
-  .catch((err)=>{
-    console.log(err);
-  });
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((err) => {
+  console.log(err);
+});

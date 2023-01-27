75f087a95da0eac4352919e8a79a84aff64334a1
Ysabel Dela Cruz <delacruz.ysa@gmail.com>
2023-01-27 13:17:43 +0800

    redirect on server site access

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ const express = require("express");
 const app = express();
 require("./db/conn");
 const cors = require("cors");
+const router =  require("./routes/messageRoute");
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/", require("./routes/messageRoute"));
+app.use("/", router)
+
+app.get("/", (req, res) => {
+  res.status(301).redirect("https://ysadc.netlify.app");
+});
 
 if(process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
